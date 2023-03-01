3c32b4f3c5a7b0a635f362343b9f51a1126fbbff
zeteticl <cougar.l@hotmail.com>
2023-03-01 17:29:57 +0800

    x

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-# Journel show online player first
+# How unfortunate are you? DiceCounting
 
-![Download](https://img.shields.io/github/downloads/hktrpg/fvtt-Journal-show-online-player-first/total)
+![Download](https://img.shields.io/github/downloads/hktrpg/How-unfortunate-are-you--Dice-Counting/total)
 <a href="https://patreon.com/HKTRPG"><img src="https://img.shields.io/endpoint.svg?url=https://shieldsio-patreon.vercel.app/api/?username=HKTRPG&type=patrons" /></a>
 
-Tiny module. Show online player first in Journal and permission control.
+Tiny module. Count
 
 ![readme](./demo01.png)
-![readme](./demo02.png)
 
 ## v1.0.0
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 Hooks.once("init", () => {
-    console.log("Show Online player First 1.0 | Initializing");
+    console.log("How unfortunate are you? DiceCounting 1.0| Initializing");
 });
 
 Hooks.on('renderDialog', () => {
@@ -14,54 +14,3 @@ Hooks.on('renderDialog', () => {
     }
 });
 
-
-Hooks.on('renderApplication', () => {
-    try {
-        const users = game.users.filter(user => user.active);
-        let playerNames = users.map(u => u.name)
-        //permission-control
-        reorderFormGroups(playerNames);
-    } catch (error) {
-        // console.log('error', error)
-    }
-});
-
-function reorderFormGroups(players) {
-    const formGroups = document.querySelectorAll('.form-group');
-    if (!formGroups) return;
-    // 找到 players 包含的 form-group
-    const selectedFormGroups = [];
-    formGroups.forEach((formGroup) => {
-        const label = formGroup.querySelector('label');
-        if (label && players.includes(label.textContent)) {
-            selectedFormGroups.push(formGroup);
-        }
-    });
-
-    // 把選中的 form-group 移动到 hidden 的后面
-    const hiddenFormGroup = document.querySelector('.form-group.hidden');
-    selectedFormGroups.forEach((formGroup) => {
-        hiddenFormGroup.parentNode.insertBefore(formGroup, hiddenFormGroup.nextSibling);
-    });
-}
-
-function sortLabel(className, playerNames) {
-    const formFields = document.querySelector(className);
-    if (!formFields) return;
-    const playerLabels = formFields.querySelectorAll('label.checkbox');
-    const playerMap = new Map();
-    // 將符合條件的玩家名稱及對應的標籤放入map中
-    playerLabels.forEach(label => {
-        const playerName = label.textContent.trim();
-        if (playerNames.includes(playerName)) {
-            playerMap.set(playerName, label);
-        }
-    });
-    // 將map中的標籤依序放在前面
-    playerNames.forEach(playerName => {
-        const label = playerMap.get(playerName);
-        if (label) {
-            formFields.insertBefore(label, formFields.firstChild);
-        }
-    });
-}
